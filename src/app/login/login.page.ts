1b8b2f170ffbeeb40a9b69ddb30d35b5e6aa84de
jhonatan-chaves <jhonatan.chaves001@gmail.com>
2023-05-29 13:03:41 -0300

    adicionando interface e validando acesso de user e senha

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TesteService } from '../services/teste.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public senha : any;
+  public pessoa: any;
+
+  constructor(public serviceService : TesteService) {
+   
+
+   }
+   validarLogin(user: string, senha : string): void{
+    const usuarioValido = this.serviceService.validarUsuario(user,senha);
+    
+    if(usuarioValido){
+      console.log("usuario valido");
+    }else{
+      console.log("usuario invalido");
+    }
+   }
 
   ngOnInit() {
+
   }
 
 }
+
--- src/app/services/teste.service.ts
+++ src/app/services/teste.service.ts
@@ -1,17 +1,96 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Usuarios } from '../login/Usuarios';
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TesteService {
 
-  constructor(private httpClient: HttpClient) { }
 
-  public getCars() : any{
-   return this.httpClient.get('https://raw.githubusercontent.com/vega/vega/master/docs/data/cars.json');
+
+  private pessoa : Array<any> = ["jhonatan", "goku", "vegeta"];
+
+  private senhas : Array< any>  = ["125", "bulma", "piculo"];
+  
+     userEsenhas : Usuarios[] = [
+    {
+      user: "jhonatan",
+      senha: "1234",
+      
+      
+      
+    },
+    {
+      user: "goku",
+      senha: "555"
+    }
+  ]
+
+
+
+
+
+   validarUsuario(user: string, senha: string): boolean {
+
+    for(const usuario of this.userEsenhas){
+      if(usuario.user === user && usuario.senha === senha){
+        console.log("verdadeiro")
+        return true;
+      }
+    }
+    console.log("falso")
+    return false;
   }
 
+
+
+  public valid2(senha: any, pessoa: any){
+  if(this.userEsenhas.findIndex(senha) == this.userEsenhas.findIndex(pessoa)){
+    console.log(this.userEsenhas.indexOf(senha));
+    console.log(this.userEsenhas.indexOf(pessoa))
+  }else{
+    console.log("alguma coisa ainda esta errado");
+  }
+  }
+
+
+
+public valid(senha: any, pessoa: any){
+  if(this.senhas.includes(senha) && this.pessoa.includes(pessoa)){
+   console.log(senha)
+   console.log(pessoa)
+    // return this.goToLogin();
+  }else{
+     this.presentToast();
+     console.log(this.senhas.values)
+   console.log(pessoa)
+  }
+
+}
+
+  constructor(private httpClient: HttpClient,  private router: Router, private toastController: ToastController, ) { 
+
+
+  }
+
+  public getCars() {
+    return this.httpClient.get('https://raw.githubusercontent.com/vega/vega/master/docs/data/cars.json');
+  }
+  /*
+  public postCars(obj:Usuarios) : any{
+   return this.httpClient.post<any>('https://raw.githubusercontent.com/vega/vega/master/docs/data/cars.json',{
+      obj
+   }).pipe(map(x=>console.log(x))).subscribe({
+  
+   })
+  }
+*/
   public setValor(valor : any){
     return valor;
   }
@@ -19,4 +98,23 @@ export class TesteService {
   public setNome(valor : any){
     return valor.include();
   }
+
+
+  
+  goToLogin(){
+    this.router.navigate(['home']);
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'E-mail ou senha, invalidos!',
+      duration: 2000,
+      position: 'top',
+    });
+
+    await toast.present();
+  }
 }
+
+
+
